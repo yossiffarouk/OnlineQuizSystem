7a88e7cc47291a47e4b6170d541286566de08105
AshrafWael <ashrafwael582@gmail.com>
2024-10-16 01:34:48 +0300

    Update UserName

--- OnlineQuiz.BLL/Dtos/StudentDtos/StudentUpdateDto.cs
+++ OnlineQuiz.BLL/Dtos/StudentDtos/StudentUpdateDto.cs
@@ -10,7 +10,7 @@ namespace OnlineQuiz.BLL.Dtos.StudentDtos
     public class StudentUpdateDto
     {
         public string Id { get; set; }
-        public string Name { get; set; }
+        public string UserName { get; set; }
         public int Age { get; set; }
         public string Grade { get; set; }
         public string ImgUrl { get; set; }
