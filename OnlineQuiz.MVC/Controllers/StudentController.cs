012f9414b3eb2fb533c2d01b2ad4aa18c315263f
KareemAymann <126060957+KareemAymann@users.noreply.github.com>
2024-10-20 03:12:24 +0300

    complete all availablequiz View&controller

--- OnlineQuiz.MVC/Controllers/StudentController.cs
+++ OnlineQuiz.MVC/Controllers/StudentController.cs
@@ -1,20 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineQuiz.BLL.Managers.Quiz;
 
 namespace OnlineQuiz.MVC.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly IQuizManager _quizManager;
+
+        public StudentController(IQuizManager quizManager)
+        {
+            _quizManager = quizManager;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
 
+  
         public IActionResult AttemptQuiz()
         {
-            return View();
-        }
+            var availableQuizzes = _quizManager.GetAvailableQuizzes().ToList();
 
+            return View(availableQuizzes);
+        }
         public IActionResult GetQuestions()
         {
             return View();
--- OnlineQuiz.MVC/Program.cs
+++ OnlineQuiz.MVC/Program.cs
@@ -1,9 +1,17 @@
 
 using Microsoft.EntityFrameworkCore;
 using OnlineQuiz.BLL.AutoMapper.AdminAutoMapper;
+using OnlineQuiz.BLL.AutoMapper.QuestionMapper;
+using OnlineQuiz.BLL.AutoMapper.QuizMapper;
 using OnlineQuiz.BLL.Managers.Admin;
+using OnlineQuiz.BLL.Managers.Base;
+using OnlineQuiz.BLL.Managers.QuestionManager;
+using OnlineQuiz.BLL.Managers.Quiz;
 using OnlineQuiz.DAL.Data.DBHelper;
 using OnlineQuiz.DAL.Repositoryies.AdminRepositroy;
+using OnlineQuiz.DAL.Repositoryies.Base;
+using OnlineQuiz.DAL.Repositoryies.QuestionRepository;
+using OnlineQuiz.DAL.Repositoryies.QuizRepository;
 
 namespace OnlineQuiz.MVC
 {
@@ -23,7 +31,18 @@ namespace OnlineQuiz.MVC
             builder.Services.AddScoped<IAdminRepositroy, AdminRepositroy>();
             builder.Services.AddScoped<IAdminManger, AdminManger>();
             builder.Services.AddAutoMapper(map => map.AddProfile(new AdminMapper()));
+            //GenericRepository && GenericManager
+            builder.Services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
+            builder.Services.AddScoped(typeof(IManager<,>), typeof(Manager<,>));
 
+            builder.Services.AddAutoMapper(map => map.AddProfile(new QuizMapper()));
+            builder.Services.AddAutoMapper(map => map.AddProfile(new QuestionMapper()));
+
+            builder.Services.AddScoped<IQuizRepository, QuizRepository>();
+            builder.Services.AddScoped<IQuestionsRepository, QuestionsRepository>();
+
+            builder.Services.AddScoped<IQuizManager, QuizManager>();
+            builder.Services.AddScoped<IQuestionManager, QuestionManager>();
             var app = builder.Build();
             
             // Configure the HTTP request pipeline.
